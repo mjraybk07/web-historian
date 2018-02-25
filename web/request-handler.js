55a97e13c50e06b5df35a11150da050847be0d32
Michael Flis <michaelflis07@gmail.com>
2018-02-24 16:48:51 -0800

    Pass first test

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,23 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var fs = require('fs');
+var httpHelpers = require ('./http-helpers');
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  console.log('handleRequest ......')
+  if (req.method === 'GET') {
+
+    fs.readFile(archive.paths.siteAssets + '/index.html', function (err, data) {
+      if ( err ) {
+        throw err;
+      } else {
+        res.writeHead(200, httpHelpers.headers);
+        res.end(data);
+      }
+    });
+
+  }
+
+  //res.end(archive.paths.list);
 };
